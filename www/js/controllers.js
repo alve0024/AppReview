d7849c70da573ba827549d2b89bb3103d7d63d07
alpsleonardo <alpsleonardo@gmail.com>
2016-11-14 21:21:19 -0500

    Implemented the Category List

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,11 +1,11 @@
 angular.module('app.controllers', [])
   
-.controller('categoryCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
-
+.controller('categoryCtrl', ['$scope', '$stateParams', 'categoryFactory', function ($scope, $stateParams, categoryFactory) {
+    categoryFactory.getAll().then(function (response) {
+        $scope.categories = response;
+    }, function (error) {
+        console.log(error)
+    });
 }])
    
 .controller('bookCategoriesCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
--- www/js/services.js
+++ www/js/services.js
@@ -1,7 +1,43 @@
 angular.module('app.services', [])
 
-.factory('BlankFactory', [function(){
-
+.factory('categoryFactory', ['$http', '$q', function($http, $q){
+    return {
+        getAll: function() {
+            var deferred = $q.defer();
+            $http.get('data.json').then(function(response){
+                deferred.resolve(response.data.categories);
+                // console.log(re)
+            }, function (error) {
+                console.log(error);
+            });
+            // CategoryService returning list of categories
+            // 	Use of promises in your service
+            return deferred.promise;
+        },
+        getCategory: function() {
+            var deferred = $q.defer();
+            $http.get('data.json').then(function(response){
+                deferred.resolve(response.data.books);
+            }, function (error) {
+                console.log(error);
+            });
+            // Returning list of products
+            // Use of promises in your service
+            return deferred.promise;
+        },
+        getProduct: function(id) {
+            var deferred = $q.defer();
+            $http.get('data.json').then(function(response){
+                var book = response.data.books;
+                deferred.resolve(book.filter(x => x._id == id));
+            }, function (error) {
+                console.log(error);
+            });
+            // Returning product lookup by Id
+            // Use of promises in your service
+            return deferred.promise;
+        }
+    }
 }])
 
 .service('BlankService', [function(){
